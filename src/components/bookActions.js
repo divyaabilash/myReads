6bfe935994345947ca9b4dfd5dfa8c786b3a0c44
Divyarajalakshmi Natarajan <divya@Abilashs-MacBook-Pro.local>
2017-09-06 13:59:08 -0700

    code review

--- src/components/bookActions.js
+++ src/components/bookActions.js
@@ -3,16 +3,21 @@ import PropTypes from 'prop-types';
 
 const BookAction = props => {
 
+
+  this.state = {value: ''};
+
   const onChanged = e => {
     props.onChange(e.target.value);
   }
   const { selected } = props;
 
-
+const onSelected = (props) =>{
+  return selected === undefined ? this.state.value : selected;
+}
   return (
 
     <div className="book-shelf-changer">
-      <select value={(selected === undefined ? 'none' : selected)} onChange={onChanged}>
+      <select value={selected === undefined ? this.state.value : selected} onChange={onChanged}>
         <option value="none" disabled>Move to...</option>
         <option value="currentlyReading">Currently Reading</option>
         <option value="wantToRead">Want to Read</option>
